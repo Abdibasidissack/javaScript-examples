e1b8b9a6ad819a11df640f92701fd117894ecb21
Spider <spider@localhost.localdomain>
2019-09-10 17:55:20 +0300

    practise website

--- 21 century microsoft/real/template-literals.js
+++ 21 century microsoft/real/template-literals.js
@@ -6,6 +6,9 @@ const job = 'Software Developer';
 const home = 'Mandera';
 let person;
 
+const name2 = 'meryan';
+const age2 = 20;
+
 // installing html from the javascript
 // without string literal
 person = '<ul> <li>Name :' + name + '</li>   <li> Age : '+ age + '</li>  <li> Job: '+ job + '</li>  <li> Home: ' + home +'</li> </ul>';
@@ -24,4 +27,8 @@ console.log(person);
 
 // with string literals 
 person = `Name ===== ${name} \nAge ===== ${age} \nJob ===== ${job} \nHome ===== ${home}`
+console.log(person);
+
+person = ` my name is : ${name2} and i am ${age2}`;
+person = ('my name is :'+ name2);
 console.log(person);
